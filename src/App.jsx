669428d0011c58eefedc0c6039937f3ffcf417f4
ShagunGarg1204 <gargshagun2005@gmail.com>
2024-06-21 15:18:56 +0530

    add intial files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from './Components/Navbar';
+import NewsBoard from './Components/NewsBoard';
+import NewsItem from './Components/NewsItem';
+import { useState } from 'react';
+const App = () => {
+  const [category,setCategory]= useState("general");
+  return (
+    <div>
+      <Navbar setCategory={setCategory}/>
+      <NewsBoard category={category}/>
+      <NewsItem/>
+    </div>
+  )
+}
+
+export default App
